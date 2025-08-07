ee6267112627b315f48da0609135b99ccf273a50
404sugar <thatonewhocaresemail@gmail.com>
2025-08-07 13:08:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ async function upload() {
         },
         body: JSON.stringify({
           message: `Update Fashion.txt with ${imgName}`,
-          content: btoa(content),
+          content: btoa(unescape(encodeURIComponent(content))),
           sha: data.sha
         })
       });
